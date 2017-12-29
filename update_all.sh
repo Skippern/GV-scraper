55a6d14945176e930be49ae9c6d11f91edf912c8
Aun Johnsen <lists@gimnechiske.org>
2017-12-29 14:15:16 +0300

    Update shell
    Update shell to ignore `README.md` and route `std.err` to `/dev/null`

--- update_all.sh
+++ update_all.sh
@@ -6,9 +6,14 @@ export back=`pwd`
 cd creators
 for i in *
     do echo "Working with $i"
+    if [ $i == "README.md" ]
+    then echo "$i is not a directory"
+        continue
+    else
     cd $i
-    ./get_duration.py
-    ./get_times.py
+    fi
+    ./get_duration.py 2>/dev/null
+    ./get_times.py 2>/dev/null
     cd ..
     done
 
